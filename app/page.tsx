ceb3c8b228d1c3936d4d07742552b46a642bb4cf
Dea314 <fabrisdanijela314@gmail.com>
2023-06-10 20:13:18 +0200

    apge err

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-/* import Container from "./components/Container";
+import Container from "./components/Container";
 import ClientOnly from "./components/ClientOnly";
 import EmptyState from "./components/EmptyState";
 import getListings, { IListingsParams } from "./actions/getListings";
@@ -8,6 +8,8 @@ import getCurrentUser from "./actions/getCurrentUser";
 interface HomeProps {
   searchParams: IListingsParams;
 }
+
+export const dynamic = "force-dynamic";
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
@@ -25,64 +27,6 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            );
-          })}
-        </div>
-      </Container>
-    </ClientOnly>
-  );
-};
-
-export default Home;
- */
-
-import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
-import EmptyState from "@/app/components/EmptyState";
-
-import getListings, { IListingsParams } from "@/app/actions/getListings";
-import getCurrentUser from "@/app/actions/getCurrentUser";
-import ClientOnly from "./components/ClientOnly";
-
-interface HomeProps {
-  searchParams: IListingsParams;
-}
-
-const Home = async ({ searchParams }: HomeProps) => {
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
-
-  if (listings.length === 0) {
-    return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
-  }
-
-  return (
-    <ClientOnly>
-      <Container>
-        <div
-          className="
-            pt-24
-            grid 
-            grid-cols-1 
-            sm:grid-cols-2 
-            md:grid-cols-3 
-            lg:grid-cols-4
-            xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8
-          "
-        >
           {listings.map((listing: any) => (
             <ListingCard
               currentUser={currentUser}
