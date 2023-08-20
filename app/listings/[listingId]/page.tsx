f8887d451c3fda95da904a3d48e4ebfcdffa1cc2
Dea314 <fabrisdanijela314@gmail.com>
2023-08-20 12:47:17 +0200

    all

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -6,18 +6,13 @@ import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 
 import ListingClient from "./ListingClient";
-import { tr } from "date-fns/locale";
 
 interface IParams {
   listingId?: string;
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-<<<<<<< HEAD
   try {
-=======
- try {
->>>>>>> 15a6e49b29af28effc26d787f5c673551e1c5199
     const listing = await getListingById(params);
     const reservations = await getReservations(params);
     const currentUser = await getCurrentUser();
@@ -41,10 +36,6 @@ const ListingPage = async ({ params }: { params: IParams }) => {
   } catch (error) {
     console.error(error);
   }
-<<<<<<< HEAD
-=======
-};
->>>>>>> 15a6e49b29af28effc26d787f5c673551e1c5199
 };
 
 export default ListingPage;
